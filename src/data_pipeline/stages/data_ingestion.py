9cebcebd75263b8eccd0d117139fef9e881d8f12
nhwolf <wolf.nicholas@outlook.com>
2025-03-14 14:09:38 -0500

    defining data read in from json

--- src/data_pipeline/stages/data_ingestion.py
+++ src/data_pipeline/stages/data_ingestion.py
@@ -1,3 +1,15 @@
 """
 Reads data in from the provided CFD json source.
 """
+
+import pandas as pd
+
+
+SOURCE_CFD_DATA = "data_source/data.json"
+
+
+def read_json_data() -> pd.DataFrame:
+    """Reads the provided CFD json file as
+    a pandas dataframe.
+    """
+    return pd.read_json(SOURCE_CFD_DATA)
