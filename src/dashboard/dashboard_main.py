578e1669a1e7561f85395b4be78084f190cd24c8
nhwolf <wolf.nicholas@outlook.com>
2025-03-14 22:52:32 -0500

    added containers for fiters and table

--- src/dashboard/dashboard_main.py
+++ src/dashboard/dashboard_main.py
@@ -17,29 +17,46 @@ with SQLiteInterface(DATABASE_PATH) as sqlite_database:
 designs = cfd_df["design"].unique().tolist()
 shapes = cfd_df["shape_id"].unique().tolist()
 
+'''
+def filter_by_design(state):
+    state.data = cfd_df[cfd_df["design"] == state.selected_design]
+    state.chart_date = (
+        state.data
+    )'
+'''
+
+
 
 with tgb.Page() as data_table_page:
-    tgb.text("# **Simulation Data**", mode="md")
-    tgb.selector(
-        value = "{designs}",
-        lov = designs,
-        dropdown = True,
-        multiple=True,
-        label = "Select Design"
+    with tgb.part(class_name="container"):
+        tgb.text("# **Simulation Data**", mode="md")
+        tgb.selector(
+            value="{designs}",
+            lov=designs,
+            #on_change=filter_by_design,
+            dropdown=True,
+            multiple=True,
+            label="Select Design"
+        )
+        tgb.selector(
+            value="{shapes}",
+            lov=shapes,
+            #on_change=filter_by_shape,
+            dropdown=True,
+            multiple=True,
+           label="Select Shape"
+        )
+    with tgb.part(class_name="container"):
+        tgb.html("br")
+        tgb.table(data="{cfd_df}")
+
+
+if __name__ == "__main__":
+    Gui(page=data_table_page).run(
+        title="CFD Dashboard",
+        use_reloader=True,
+        port="auto",
+        debug=True,
+        watermark="", # removes watermark
+        margin="4em"
     )
-    tgb.selector(
-        value = "{shapes}",
-        lov = shapes,
-        dropdown = True,
-        multiple=True,
-        label = "Select Shape"
-    )
-    tgb.html("br")
-    tgb.table(data="{cfd_df}")
-
-
-
-Gui(page=data_table_page).run(
-    title="CFD Dashboard",
-    dark_mode=False,
-    auto_port=True)
