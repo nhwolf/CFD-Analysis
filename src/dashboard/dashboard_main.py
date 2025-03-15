cbafcb45a5f0959bece681a59e109e21b11c80b9
nhwolf <wolf.nicholas@outlook.com>
2025-03-15 01:15:21 -0500

    moved filtering to utility file

--- src/dashboard/dashboard_main.py
+++ src/dashboard/dashboard_main.py
@@ -4,12 +4,13 @@ Main dashboard file to render the dashboard data
 
 import pandas as pd
 
-from taipy.gui import Gui, State
+from taipy.gui import Gui, State, Icon, navigate
 import taipy.gui.builder as tgb
 
 from data_utils.sqlite_interface import SQLiteInterface
 from data_utils import queries
 from data_utils.database_path import DATABASE_PATH
+from page_utils import data_table_page_utils
 
 
 with SQLiteInterface(DATABASE_PATH) as sqlite_database:
@@ -26,24 +27,29 @@ selected_shapes = shapes.copy()
 filtered_df = cfd_df.copy()
 
 
-def filter_data(state: State):
-    df = cfd_df.copy()
-    if state.selected_designs:
-        df = df[df["design"].isin(state.selected_designs)]
-    if state.selected_shapes:  # Only apply filter if selection exists
-        df = df[df["shape_id"].isin(state.selected_shapes)]
-    state.filtered_df = df
+def menu_option_selected(state, action, info):
+    page = info["args"][0]
+    navigate(state, to=page)
 
+with tgb.Page() as root_page:
+    tgb.toggle(theme=True)
+    tgb.menu(
+        label="Menu",
+        lov=[
+            ("page1", Icon("images/data_table.png", "Data Table")),
+        ],
+        on_action=menu_option_selected,
+    )
 
 with tgb.Page() as data_table_page:
-    tgb.toggle(theme=True)
-    tgb.text("# **Simulation Data**", mode="md")
+    with tgb.part(class_name="container"):
+        tgb.text("# **Simulation Data**", mode="md")
     with tgb.part(class_name="container", columns="1 1"):
         with tgb.layout("1 1"):
             tgb.selector(
                 value="{selected_designs}",
                 lov=designs,
-                on_change=filter_data,
+                on_change=lambda state: data_table_page_utils.filter_data(state, cfd_df),
                 dropdown=True,
                 multiple=True,
                 label="Select Design",
@@ -52,18 +58,34 @@ with tgb.Page() as data_table_page:
             tgb.selector(
                 value="{selected_shapes}",
                 lov=shapes,
-                on_change=filter_data,
+                on_change=lambda state: data_table_page_utils.filter_data(state, cfd_df),
                 dropdown=True,
                 multiple=True,
                 label="Select Shape",
-                hover_text="Filter table by design"
+                hover_text="Filter table by shape"
             )
     with tgb.part(class_name="container"):
         tgb.html("br")
         tgb.table(data="{filtered_df}")
 
+def menu_option_selected(state, action, info):
+    page = info["args"][0]
+    navigate(state, to=page)
+
+with tgb.Page() as root_page:
+    tgb.toggle(theme=True)
+    tgb.menu(
+        label="Menu",
+        lov=[
+            ("page1", Icon("images/data_table.png", "Data Table")),
+        ],
+        on_action=menu_option_selected,
+    )
+
+pages = {"/": root_page, "page1": data_table_page}
+
 if __name__ == "__main__":
-    Gui(page=data_table_page).run(
+    Gui(pages=pages).run(
         title="CFD Dashboard",
         use_reloader=True,
         port="auto",
