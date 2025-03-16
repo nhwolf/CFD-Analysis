eea7b79713b6a7da77240d28b8ad648ad73b9037
nhwolf <wolf.nicholas@outlook.com>
2025-03-16 15:35:16 -0500

    adding placeholder for create_db module unit tests

---
+++ tests/database/test_create_db.py
@@ -0,0 +1,11 @@
+"""
+Unit tests for the create_db.py module.
+"""
+
+from database.create_db import create_database, create_table
+
+def test_create_database():
+    pass
+
+def test_create_table():
+    pass
